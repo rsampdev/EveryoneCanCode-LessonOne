5df065c9c6981191113716916bb64b33503c9b78
rsampdev <rsampdev@gmail.com>
2016-09-01 14:24:33 -0400

    Initial commit

---
+++ 01_PlaygroundBasics.playground/Pages/03-Calculations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: ## Calculations
+//: You can use a playground like a calculator. Type in the math problem you want solved and the results sidebar shows the answer, or the _result_, of the calculation.
+//:
+//: You can use the plus `+` and minus `-` signs for addition and subtraction just as you’d expect:
+34 + 56 + 230
+
+1000 - 300
+
+34768592 + 7284930
+
+37564897 - 47368
+//: For multiplication, use the asterisk `*` :
+321 * 1005674389
+//: For division, use the slash `/` :
+24 / 8
+
+27 / 3
+
+6789 / 47
+//: For grouping, use parenthesis `( )` :
+(300 + 500 + 400) / 3
+
+(566789 + 56789) * (526738 / 43) + (5674839 * 7)
+//: - experiment:\
+//: Practice using the playground like a calculator.
+//: Edit some of the existing expressions and a few of your own.
+//: Notice every time you make a change the results are updated in the sidebar.
+//:
+//:[Previous](@previous)  |  page 3 of 7  |  [Next: Comments](@next)
---
+++ 01_PlaygroundBasics.playground/Pages/04-Comments.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: ## Comments
+//: Programmers leave little notes right in the code to help explain it.
+//: This helps people understand the code when they read it later.
+//: These notes are called _comments_.
+//:
+//: Comments are ignored by the playground and do not affect how your code is run.
+//: 
+//: A comment starts with two slashes `//` :
+// This is a comment, it does not affect the code
+// 34 + 56 + 230
+//: Notice that because comments are ignored, no result appears in the results bar for that line of code.
+28493 + 34 + 457289
+
+10 + 20 + 30 / 40
+
+(67438 * 4874837 * 58379) / 435
+//: - experiment:\
+//: Try _commenting out_ the calculation `34 + 56 + 230` by adding two slashes to the beginning of the calculation.\
+//:Try _uncommenting_ the calculation `200 + 34 + 45` by deleting the two slashes.\
+//:Edit some of the existing expressions and a few of your own.\
+//:Notice every time you make a change the results are updated in the sidebar.
+//:
+//:[Previous](@previous)  |  page 4 of 7  |  [Next: When things go wrong 😰](@next)
